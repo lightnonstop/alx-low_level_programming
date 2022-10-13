7b6aa33c7a7ccf8569d820d7dfe7a6bf115c98f4
lightnonstop <umichaeledet003@gmail.com>
2022-10-13 13:22:12 -0700

    Added a script that prints a string followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int main(void)
-{
-/** 
- * main - prints string with newline 
+/**
+ * main - prints string with newline
  * Description: using the main function
  * program prints "Programming is like building a multilingual puzzle
  * Return: 0
-*/
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
