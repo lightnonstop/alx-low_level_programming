0b9ab1e7089ca0aa0caeb0b243c3c2c472c5e3a6
lightnonstop <umichaeledet003@gmail.com>
2022-10-13 13:50:28 -0700

    Added script that prints the size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - accepts no arguments but prints byte values
+ * Description: integer values have been declared
+ * and bytes size values assigned for output process
+ * Return: 0
+*/ 
+int main(void)
+{
+int size_of_char = sizeof(char);
+int size_of_int = sizeof(int);
+int size_of_long_int = sizeof(long int);
+int size_of_long_long_int = sizeof(long long int);
+int size_of_float = sizeof(float);
+printf("Size of a char: %d byte(s)\n", size_of_char);
+printf("Size of an int: %d byte(s)\n", size_of_int);
+printf("Size of a long int: %d byte(s)\n", size_of_long_int);
+printf("Size of a long long int: %d byte(s)\n", size_of_long_long_int);
+printf("Size of a float: %d byte(s)\n", size_of_float);
+return (0);
+}
