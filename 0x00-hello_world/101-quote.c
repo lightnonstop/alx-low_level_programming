6d36f2ea38079ce79c7c90dd79691eb2bd6fad3a
lightnonstop <umichaeledet003@gmail.com>
2022-10-13 14:32:41 -0700

    Added script that prints message to standard error output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 /**
  * main: uses output message function
  * Description: uses stderr and outputs message to the screen
+ * stderr meassages can only be displayed on the console
  * Return: 1
  */ 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar
+, 2015-10-19\n");
 return (1);
 }
