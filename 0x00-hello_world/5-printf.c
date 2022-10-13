053cfa29ab9b7b85385072d15aec15e8e4d8d076
lightnonstop <umichaeledet003@gmail.com>
2022-10-13 13:31:13 -0700

    Added script that prints a string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints string, adds new line
+ * Description: using the printf function to
+ * print "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
