77ba65ab84789f49685e85ae2754eefb9cdc4134
lightnonstop <umichaeledet003@gmail.com>
2022-10-16 07:42:47 -0700

    Added a program that uses putchar function to output asii characters 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
  * main - starting point function
  * Description: a program that prints all single digit numbers of
  * base 10 starting from 0.
- * putchar accepts char and works as an integer.
+ * putchar accepts character set.
  * Return: 0
 */
 int main(void)
