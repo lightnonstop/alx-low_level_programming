483bde0033c4f9bdce2366ce2e4488ec874133f5
lightnonstop <umichaeledet003@gmail.com>
2022-10-16 08:40:34 -0700

    Added a c program that prints z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** main - starting point function
+ * Description: prints characters from z to a (backwards) with a newline
+ * putchar function accepts a one character argument
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
