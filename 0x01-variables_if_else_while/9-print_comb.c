2dc6feca95198055b451c45ba74aff8fd30b50cc
lightnonstop <umichaeledet003@gmail.com>
2022-10-16 09:47:51 -0700

    Added a program that will output from 0 to 9 with comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - starting point function
+ * Description: prints numbers 0 to 9
+ * Use comma and space after each number output
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++)
+{
+if (num == 57)
+{
+putchar(num);
+}
+else
+{
+putchar(num);
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
