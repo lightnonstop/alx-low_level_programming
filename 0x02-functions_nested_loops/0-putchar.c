2fae9d0f655fb9bd7471685caee8b14876dc15cc
lightnonstop <umichaeledet003@gmail.com>
2022-10-18 06:02:05 -0700

    updated source file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
   * main - starting point function
   * Description: prints a string and a new line
@@ -7,6 +6,14 @@
   */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
