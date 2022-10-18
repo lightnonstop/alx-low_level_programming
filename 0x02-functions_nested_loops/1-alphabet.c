d5ba427c5a628f60e4f18d0e8792e7423a9b303b
lightnonstop <umichaeledet003@gmail.com>
2022-10-18 12:49:53 -0700

    Added source file and header file for printing alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+  * print_alphabet - prints the alphabet in lowercase
+  * Description - uses _putchar function from main.h library
+  * Return: 0 (Success)
+  */
 void print_alphabet(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha < 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
