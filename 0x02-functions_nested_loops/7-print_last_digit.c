351a0e4e90136e7e7261cd65c894911b76225c00
lightnonstop <umichaeledet003@gmail.com>
2022-10-19 16:14:19 +0100

    Added program for the last digit of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_last_digit - prints the last digit of an integer
+  * @n - First Operand
+  * Description: Divides integer argument by base ten
+  * Remainder of the division is the last digit of the integer
+  * Return: 0 (Success)
+  */
+int print_last_digit(int)
+{
+	int a;
+
+	a = n % 10;
+	_putchar(a);
+	return (0);
+}
