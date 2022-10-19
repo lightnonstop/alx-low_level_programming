157ff0db2eaa52076a5075270bf555fb7018b41a
lightnonstop <umichaeledet003@gmail.com>
2022-10-19 16:55:48 +0100

    Updated program source file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,21 @@
-#include "main.h"
+#include <stdio.h>
 /**
   * print_last_digit - prints the last digit of an integer
   * @n: First Operand
-  * Description: iDivides integer argument by base ten
-  * Remainder of the division is the last digit of the integer
+  * Description: Divides integer argument by base ten
+  * Remainder of the division is added to character 0 for 
+  * integer to character conversion
   * Return: 0 (Success)
   */
-int print_last_digit(int n)
+int print_last_digit(int)
 {
-	int a;
-
-	a = n % 10;
-	_putchar(a);
+	if (n > 0)
+	{
+		putchar(-n%10 + '0');
+	}
+	else
+	{
+		putchar(n%10 + '0');
+	}
 	return (0);
 }
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+/**
+  * print_last_digit - prints the last digit of an integer
+  * @n: First Operand
+  * Description: iDivides integer argument by base ten
+  * Remainder of the division is the last digit of the integer
+  * Return: 0 (Success)
+  */
 int main(void)
 {
-	int n = -767, a;
-	if (n < 0)
+	if (n > 0)
 	{
-		a = n*-1;
-	}
-	else if (n > 0)
-	{
-		a = n;
-	}
-	printf("%d",a);
+	putchar(-n%10 + '0');
 	return (0);
+	}
+	else
+	{
+		putchar(n%10 + '0');
+	}
 }
