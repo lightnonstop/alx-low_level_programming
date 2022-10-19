b378aa04263ac1acabd84b1eab46e540ac5ceaa9
lightnonstop <umichaeledet003@gmail.com>
2022-10-19 16:19:31 +0100

    Updated program source file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
   * print_last_digit - prints the last digit of an integer
-  * @n - First Operand
-  * Description: Divides integer argument by base ten
+  * @n: First Operand
+  * Description: iDivides integer argument by base ten
   * Remainder of the division is the last digit of the integer
   * Return: 0 (Success)
   */
