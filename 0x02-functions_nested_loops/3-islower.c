93051a146478ab457863fe9f931f05f6e19c632e
lightnonstop <umichaeledet003@gmail.com>
2022-10-19 11:03:43 +0100

    Added a new program

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * _islower - checks if a value is lowercase
+  * Description: porgram uses int data types
+  * and breaks if the value is lowercase
+  * Return: 1 (Success) or 0 (Failure)
+  */
+int _islower(int c)
+{
+	int ret, alpha;
+	for (alpha = 97; alpha <= 122; alpha++)
+	{
+		if (c != alpha)
+		{
+			ret = 0;
+		}
+		else
+		{
+			ret = 1;
+			break;
+		}
+	}
+	return ret;
+}
+
