808fe7a73f0be2ccef58b9f49afd5c8596fd2d9d
lightnonstop <umichaeledet003@gmail.com>
2022-10-20 12:09:11 +0100

    Testing program function to print positive or negative for an integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * main - testing function that prints if an integer is positive or negative
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	int p = 0;
+       	positive_or_negative(p);
+	return (0);
+}
