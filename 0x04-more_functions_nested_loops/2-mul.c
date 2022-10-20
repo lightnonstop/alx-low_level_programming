10cefd07e90946f503da4eeb378d4cd994efd9cb
lightnonstop <umichaeledet003@gmail.com>
2022-10-20 23:20:30 +0100

    Program that computes the product of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * mul - Computes the product of two integers;
+  * @a: First Operand
+  * @b: Second Operand
+  * Return: Product of the two integers
+  */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
