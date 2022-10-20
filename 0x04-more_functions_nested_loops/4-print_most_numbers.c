955d757b622828caacfdab2c77156ef2fb358eef
lightnonstop <umichaeledet003@gmail.com>
2022-10-20 23:41:28 +0100

    Skip 2 and 4 program updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
   * print_most_numbers - Outputs numbers from 0 to 9 skips 2 and 4
+  * Description: Skips 2 and 4 values when loop reaches these points
   */
 void print_most_numbers(void)
 {
