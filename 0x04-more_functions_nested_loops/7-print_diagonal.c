43e83511250b8b937a3c6c32472cdebec4585a58
lightnonstop <umichaeledet003@gmail.com>
2022-10-21 10:52:34 +0100

    Removed trailing spaces from program

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,8 +4,8 @@
   * @n: Integer argument
   * Description: First loop iterates vertically then the second loop
   * iterates horizontally with the new line put in consideration.
-  * i.e As the first loop picks up a value that is 
-  * less than n - 1, the second loop iterates on that value (Prints output 
+  * i.e As the first loop picks up a value that is
+  * less than n - 1, the second loop iterates on that value (Prints output
   * for the number(s) of times).
   */
 void print_diagonal(int n)
