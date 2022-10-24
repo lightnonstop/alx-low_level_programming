ae640470ca32a7edc8b810c24505c909c7920d61
lightnonstop <umichaeledet003@gmail.com>
2022-10-25 00:36:09 +0100

    Added a program reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * rev_string - Reverses a string
+  * @s: Charater argument
+  */
+void rev_string(char *s)
+{
+	int i, length = strlen(s);
+
+	for (i = length; i >= 0; i--)
+	{
+		*s = s[i];
+	}
+}
