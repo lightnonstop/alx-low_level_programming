d52dfa7541ef7637053c44ffa402123d9e89728c
lightnonstop <umichaeledet003@gmail.com>
2022-10-24 18:24:16 +0100

    Corrected program to compromise betty standard

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+  * reset_to_98 -  Updates the value it points to to 98.
+  * @n: Pointer Argument
+  * Description: Takes a pointer to an int as parameter and
+  * updates the value of the variable it points to to 98.
+  */
 void reset_to_98(int *n)
 {
 	*n = 98;
