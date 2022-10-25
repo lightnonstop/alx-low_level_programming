ba0f59c1bad71bb93bf3b9928da45272b167ea06
lightnonstop <umichaeledet003@gmail.com>
2022-10-25 15:41:28 +0100

    Made corrections on source file andand added program that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,15 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-  * rev_string - Reverses a string
-  * @s: Charater argument
+  * rev_string - Reverses a given string
+  * @s: Charater pointer argument
+  * Description: Loop swaps two string characters simultaneously end to end.
+  * Until mo more possible swaping.
   */
 void rev_string(char *s)
 {
-	int i, length = strlen(s);
-
-	for (i = 0; i < length - 1; i++)
+	int length = strlen(s), i;
+	char hold;
+	for (i = 0; i < (length / 2); i++)
 	{
-		strncat(*s, s[i], length);
+		hold = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = hold;
 	}
 }
