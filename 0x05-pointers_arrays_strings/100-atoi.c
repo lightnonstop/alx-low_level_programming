39fca9f908ffc9f2fdf403cbaee977578ead743f
lightnonstop <umichaeledet003@gmail.com>
2022-10-26 16:42:57 +0100

    Added a program that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+  * _atoi - Converts an integer to a string.
+  * @s: First Operand
+  * Return: Integer (Success) or 0 (Failure)
+  */
+int _atoi(char *s)
+{
+	int new;
+
+	new = atoi(s);
+	return (new);
+}
