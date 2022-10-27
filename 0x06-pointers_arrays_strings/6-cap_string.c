c97edde427a4606b8d041568e2551c89c206084c
lightnonstop <umichaeledet003@gmail.com>
2022-10-27 22:33:59 +0100

    Added c program that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+  * cap_string - Changes all lowercase letters of a string to uppercase.
+  * @str: First Operand
+  * Description: Checks if each character in the string
+  * is between a to z lowercase the subtracts 32 from their ascii character
+
+  * value to obtain the uppercase of the character.
+  * Return: Pointer string
+  */
+char *cap_string(char *s)
+{
+	int i, length = strlen(s);
+	for (i = 0; i < length; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+			continue;
+		}
+		if (s[i] == '.' || s[i] == '\t' || s[i] == '\n' || s[i] == ' ')
+		{
+			++i;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				continue;
+			}
+			else if (s[i] == '.' || s[i] == '\t' || s[i] == '\n' || s[i] == ' ')
+			{
+				++i;
+				if (s[i] >= 'a' && s[i] <= 'z')
+				{
+					s[i] = s[i] - 32;
+					continue;
+				}
+			}
+		}
+	}
+	return (s);
+}
