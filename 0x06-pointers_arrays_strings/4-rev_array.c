0ef9799b39a489868482910f2d38045e07428896
lightnonstop <umichaeledet003@gmail.com>
2022-10-27 05:36:58 -0700

    Fixed some bugs and removed executable files

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,8 +14,8 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < (n / 2); i++)
 	{
-		hold = a[i];
+		hold_int = a[i];
 		a[i] = a[n - i - 1];
-		a[n - i - 1] = hold;
+		a[n - i - 1] = hold_int;
 	}
 }
