35a4c5dd4d4a40a75d9345978a117a34a13a328b
lightnonstop <umichaeledet003@gmail.com>
2022-10-27 03:07:51 -0700

    Fixed betty error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,7 @@
   * @s1: First Operand
   * @s2: Second Operand
   * Return: 0 if both strings are equal | >0
-  * if the first not matching character in first string has the greater 
+  * if the first not matching character in first string has the greater
   * ASCII value than the corresponding character in the second string | <0
   * if the first not matching character in first string has the lesser
   * ASCII value than the corresponding character in the second string
@@ -15,6 +15,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int res;
+
 	res = strcmp(s1, s2);
 	return (res);
 }
