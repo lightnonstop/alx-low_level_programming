0dc37958b1b9810141723ac2ec371f2fc87fb555
lightnonstop <umichaeledet003@gmail.com>
2022-10-31 10:58:43 +0100

    Minor alterations in c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,9 @@
  * @n: Size of memory to be filled in bytes
  * Return: s
  */
-char *_memset(char *s, char b, unsigned int n){
+char *_memset(char *s, char b, unsigned int n)
+{
 	memset(s, b, n);
 	return (s);
 }
+
