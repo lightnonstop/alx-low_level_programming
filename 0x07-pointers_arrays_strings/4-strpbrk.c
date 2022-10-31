4f9107c4da47cbddf3c14f537336fa9ccd7773d6
lightnonstop <umichaeledet003@gmail.com>
2022-10-31 13:27:14 +0100

    Added function that searches for the first occurrence of a set of bytes in a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ *_strpbrk - Searches a string for any of a set of bytes.
+ * @s: Pointer to string to be checked
+ * @accept: Character to be searched in s
+ * Return: Pointer to the bytes in s that matches
+ * first occurrence of any bytes in the accept.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char chr = strpbrk(s, accept);
+
+	return (chr);
+}
