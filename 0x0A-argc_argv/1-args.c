2cfbb92018732117566c98456f11da810f445e6b
lightnonstop <umichaeledet003@gmail.com>
2022-11-05 06:23:19 +0100

    Fixed unused variable error

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
   * @argv: The array of command line argument
   * Return: 0 for success
   */
-int main(int argc, char **argv)
+int main(int argc, __attribute__ ((unused)) char **argv)
 {
 	printf("%d\n", argc - 1);
 	return (0);
