d7cbcb3b346d3e69c240112fdc35761a7adfccc3
lightnonstop <umichaeledet003@gmail.com>
2022-11-05 21:29:44 +0100

    Removed error code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,30 +10,19 @@
   */
 int main(int argc, char **argv)
 {
-	int i, ret = 1, sum = 0;
+	int i, sum = 0;
 
 	if (argc < 1)
+		return (0);
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (!atoi(argv[i]))
 		{
-			if (atoi(argv[i]) > 0)
-			{
-				sum += atoi(argv[i]);
-					if (i == argc - 1)
-					{
-						printf("%d\n", sum);
-					}
-			}
-			else if (atoi(argv[i]) == 0)
-			{
-				printf("Error\n");
-				ret = 1;
-				break;
-		}	}
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
 	}
-	return (ret);
+	printf("%d\n", sum);
+	return (0);
 }
