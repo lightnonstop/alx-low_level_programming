72cd3927ea5f59202c1824647366bdd26830a513
lightnonstop <umichaeledet003@gmail.com>
2022-11-05 06:26:43 +0100

    Fixed error of unused variable

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
   *
   * Return: 0 (Success)
   */
-int main(int argc, char **argv)
+int main(__attribute__ ((unused)) int argc, char **argv)
 {
 	if (argv[1] && argv[2])
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
