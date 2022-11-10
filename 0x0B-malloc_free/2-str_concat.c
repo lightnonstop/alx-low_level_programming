48974743a80464f86a6504793672d6c3b6656a18
lightnonstop <umichaeledet003@gmail.com>
2022-11-10 11:05:45 +0100

    Output error fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i = 0, j = 0, total;
 
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	{
+		s1 = "";
+		s2 = "";
+	}
 	else if (s1 == NULL && s2 != NULL)
 		s1 = "";
 	else if (s2 == NULL && s1 != NULL)
