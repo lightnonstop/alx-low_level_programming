e26c68e2bfcce246126a65ca8bfa66e92e514c42
lightnonstop <umichaeledet003@gmail.com>
2022-11-10 14:33:03 +0100

    Fixed syntax error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,8 +20,10 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
+	{
 		free(arr);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		arr[i]  = (int *)malloc(width * sizeof(int));
