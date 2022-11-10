a742a8b1294a6c517346585f0468c9c1eea91866
lightnonstop <umichaeledet003@gmail.com>
2022-11-10 14:28:10 +0100

    Added a program that allocates memory for array of arrays

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+  * alloc_grid - Creates a 2 dimensional array of integers.
+  * @width:  Number of columns of array
+  * @height: Number of rows of array
+  * Description: Allocates dynamic memory to array of 1D arrays of integers.
+  *
+  * Return: Pointer to 2D array, NULL if malloc is NULL, NULL if row or
+  * column of array is negative or zero.
+  */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i]  = (int *)malloc(width * sizeof(int));
+	}
+		return (arr);
+}
