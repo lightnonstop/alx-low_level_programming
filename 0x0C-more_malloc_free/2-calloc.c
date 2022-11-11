9c7287ec3b55eba26c09af13e4103dceb4ee3e8e
lightnonstop <umichaeledet003@gmail.com>
2022-11-11 13:25:26 +0100

    Program allocates memory for an array when number of elements and size of each element is given

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	while (i < size)
+	{
+		arr[i] = 0;
+		i++;
+	}
 	return (arr);
 }
