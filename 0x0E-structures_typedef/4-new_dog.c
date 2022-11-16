ad7d9cfb7def45bf4ad0996cd929d7b586458a03
lightnonstop <umichaeledet003@gmail.com>
2022-11-16 13:04:36 +0100

    Made my own code syntax changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,44 +10,33 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	dog_t *p;
 	int i, name_len, owner_len;
 
 	name_len = 0;
 	owner_len = 0;
-	ptr = malloc(sizeof(*ptr));
-
-	if ((ptr == NULL) || !(name) || !(owner))
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		free(p);
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		name_len++;
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_len++;
+	p->name = malloc(name_len + 1);
+	p->owner = malloc(owner_len + 1);
+	if (!(p->name) || !(p->owner))
 	{
-		free(ptr);
+		free(p->name);
+		free(p->owner);
+		free(p);
 		return (NULL);
 	}
-
-	for (i = 0; name[i]; i++)
-		name_len += 1;
-
-	for (i = 0; owner[i]; i++)
-		owner_len += 1;
-
-	ptr->name = malloc(name_len + 1);
-	ptr->owner = malloc(owner_len + 1);
-
-	if (!(ptr->name) || !(ptr->owner))
-	{
-		free(ptr->name);
-		free(ptr->owner);
-		free(ptr);
-		return (NULL);
-	}
-
 	for (i = 0; i < name_len; i++)
-		ptr->name[i] = name[i];
-	ptr->name[i] = '\0';
-
-	ptr->age = age;
-
+		p->name[i] = name[i];
+	p->name[i] = '\0';
 	for (i = 0; i < owner_len; i++)
-		ptr->owner[i] = owner[i];
-	ptr->owner[i] = '\0';
-	return (ptr);
+		p->owner[i] = owner[i];
+	p->owner[i] = '\0';
+	return (p);
 }
