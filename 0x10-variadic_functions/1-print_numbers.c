bbfe4a01afc1a2823df2cef6ed3d9d343c0954a7
lightnonstop <umichaeledet003@gmail.com>
2022-11-18 01:10:43 +0100

    Added program that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+  * print_numbers - Prints numbers
+  * @separator: Comma and space separator
+  * @n: Number of variable arguments passed
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+	va_start(arg, n);
+
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d%s", va_arg(arg, int), separator);
+		else
+			printf("%d\n", va_arg(arg, int));
+	}
+	va_end(arg);
+}
