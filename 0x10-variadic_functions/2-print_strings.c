efae68a39d6110a45e1c2b8bb4e3c87e99ba9a6c
lightnonstop <umichaeledet003@gmail.com>
2022-11-18 02:51:46 +0100

    Added program that prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+  * printf_strings - Prints strings
+  * @separator: Separator to be used after string
+  * @n: Number of variable string arguments passed
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+	char *s;
+
+	va_start(arg, n);
+
+	if (!separator)
+		separator = "";
+	if (n == 0)
+		exit(0);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(arg, char*);
+		if (i < n - 1)
+		{
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s%s", s, separator);
+		}
+		else
+		{
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s\n", s);
+		}
+	}
+	va_end(arg);
+}
