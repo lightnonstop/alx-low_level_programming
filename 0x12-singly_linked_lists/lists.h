76a837ae4cb5abba506dd107d137296a408d5ffd
lightnonstop <umichaeledet003@gmail.com>
2022-11-26 05:57:04 +0100

    Updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,10 +15,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 #include <string.h>
+
 void _putchar(char n);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
