6fef6437872db0a2b28ddcd6ee06841f2ad34be7
lightnonstop <umichaeledet003@gmail.com>
2022-11-28 14:42:28 +0100

    Function frees a list completely

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+  * free_listint2 - Frees a list completely
+  * @head: Pointer to a starting node
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	while (*head)
+	{
+		new = *head;
+		*head = (*head)->next;
+		free(new);
+	}
+}
