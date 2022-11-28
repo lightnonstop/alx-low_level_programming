9282f4fdfcb8c0b8166f72349db22fdb03d378b8
lightnonstop <umichaeledet003@gmail.com>
2022-11-28 15:49:20 +0100

    Function determines the nth position node of a linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+  * get_nodeint_at_index - Determines the nth node of a linked list
+  * @head: Pointer to starting node
+  * @index: Node position starting from zero (0)
+  * Return: The nth node of a linked list, NULL if the node does not exist
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int times;
+	listint_t *new, *temp;
+
+	times = 1;
+	if (head == NULL)
+		return (NULL);
+	new = head;
+	while (times < index + 2)
+	{
+		temp = new;
+		new = new->next;
+		times++;
+	}
+	return (temp);
+}
