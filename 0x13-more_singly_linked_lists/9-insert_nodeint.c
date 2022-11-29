121918c90f356830666c8d823a8a67129bd4609b
lightnonstop <umichaeledet003@gmail.com>
2022-11-29 14:26:03 +0100

    Error fixing but on trial

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				new->next = temp->next;
 				temp->next = new;
 				new->n = n;
-				return (new);
+				return (*head);
 			}
 			temp = temp->next;
 			nth++;
