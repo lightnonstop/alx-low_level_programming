fdbabfa7b5ed2a7cb0b1e49f9ae8a0fd5984131c
lightnonstop <umichaeledet003@gmail.com>
2022-11-29 03:55:05 +0100

    Error fixed on trial

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,7 +24,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp->next = new;
 	temp = new;
 	new->n = n;
-	if (*head == NULL)
-		return (NULL);
-	return (*head);
+	return (new);
 }
