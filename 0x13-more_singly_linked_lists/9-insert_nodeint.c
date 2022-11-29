322835c7e68123a6e28fb3006091b8a06f672aad
lightnonstop <umichaeledet003@gmail.com>
2022-11-29 20:47:15 +0100

    Fixed error in code completely

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,9 +42,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		nth++;
 	}
-/**
-  * 	if (idx > nth)
-  *		return (NULL);
-*/
 	return (new);
 }
