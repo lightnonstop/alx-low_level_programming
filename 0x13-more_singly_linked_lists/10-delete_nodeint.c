3025d78a7a8c0245ae107b65e0e67eebae16d11f
lightnonstop <umichaeledet003@gmail.com>
2022-11-30 02:08:51 +0100

    Fixed error with function that deletes a node at a particular position in a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(new);
 		return (1);
 	}
-	while (nth = 0; nth < index - 1; nth++)
+	for (nth = 0; nth < index - 1; nth++)
 	{
 		new = new->next;
 		if (new == NULL)
