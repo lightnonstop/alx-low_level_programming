c28b892b748df7936d3b4c5614f50fb449d06a68
lightnonstop <umichaeledet003@gmail.com>
2022-12-02 13:06:44 +0100

    Determines the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,12 @@
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(*n) * 8))
+	if (index >= (sizeof(n) * 8))
 		return (-1);
 
-	*n |= 1 << index;
-	return (1);
+	n = n >> index;
+	if (n & 1)
+		return (1);
+	else
+		return (0);
 }
