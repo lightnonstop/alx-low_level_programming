e022015d8c27e198952ed3f666de338f489e7e8a
lightnonstop <umichaeledet003@gmail.com>
2022-12-02 12:59:17 +0100

    Determines the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
-  * count_bit - Counts the number of bits of a number in binary
-  * @num: The number
-  * Return: Number of bits counted
-  */
-unsigned int count_bit(unsigned int num)
-{
-	unsigned int count;
-
-	count = 0;
-	while (num != 0)
-	{
-		num /= 2;
-		count++;
-	}
-	return (count);
-}
-/**
   * get_bit - Determines the value of a bit at a given index
   *
   * @n: Given number
@@ -27,21 +10,12 @@ unsigned int count_bit(unsigned int num)
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int *new;
-	unsigned int bits, i = 0;
+	unsigned int bits;
 
-	bits = count_bit(n);
-	new = malloc(sizeof(int) * bits);
-	bits--;
-	while (n != 0 && i <= bits)
-	{
-		new[i] = n & 1;
-		if (i == index)
-			return (new[i]);
-		if (i == bits && index > i)
-			return (0);
-		n = n >> 1;
-		i++;
-	}
-	return (-1);
+	bits = (n >> index) & 1;
+
+	if ((bits == 1) || (bits == 0)
+		return (bits);
+	else
+		return (-1);
 }
