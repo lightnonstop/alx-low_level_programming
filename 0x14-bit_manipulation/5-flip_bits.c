9a90cf32a6a4c9f65c4d2083c3fe0ddf8d07b8da
lightnonstop <umichaeledet003@gmail.com>
2022-12-02 23:43:14 +0100

    Determines the number of bits needed to be flipped to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * flip_bits - Determines the number of bits needed to flip
+  * to get from one number to another.
+  *
+  * @n: First Operand
+  * @m: Second Operand
+  *
+  * Description: To get the second number from the first number,
+  * some bit values have to be interchanged (i.e for consecutive bits which are
+  * binary opposite in value, maybe for one, 1 bit and the other 0 bit)
+  * until the second number in binary is derived successfully.
+  * The number of interchanges is done with the help
+  * of a bit manipulation operator (^) and loop for the counting each flip
+  *
+  * Return: Number of bits needed to be flipped
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int new, a, count;
+
+	new = n ^ m;
+	count = 0;
+	while (new != 0)
+	{
+		a = new & 1;
+		if (a == 1)
+			count++;
+		new = new >> 1;
+	}
+	return (count);
+}
