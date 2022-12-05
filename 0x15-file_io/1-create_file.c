1332cb7d4cb3ae985e5f890204d0db4b9b136049
lightnonstop <umichaeledet003@gmail.com>
2022-12-06 00:43:49 +0100

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		count++;
 		i++;
 	}
-	fd = open(filename, O_RDWR | O_CREAT, O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	wr = write(fd, text_content, count);
