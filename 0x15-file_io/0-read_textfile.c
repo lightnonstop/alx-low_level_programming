cd68cbb19d8b9982aedccf4a23ac01c42749ef6e
lightnonstop <umichaeledet003@gmail.com>
2022-12-05 22:30:21 +0100

    Reads a text file and prints it to the standard output, error fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,30 +18,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+	buff = malloc(sizeof(char) * letters);
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * (letters + 1));
-
-	if (buff == NULL)
-		return (0);
-
 	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
-		free(buff);
 		return (0);
 	}
 
-	buff[letters] = '\0';
-
 	wr = write(STDOUT_FILENO, buff, rd);
+
 	if (wr == -1)
 		return (0);
-	close(fd);
-	free(buff);
 	return (wr);
 }
