01acd8f38f8ed652c496e603f4031672288472d9
lightnonstop <umichaeledet003@gmail.com>
2022-12-05 22:02:38 +0100

    Header file updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
-#define MIAN_H
-#include <stdlib.h>
-#include <stdio.h>
+#define MAIN_H
+
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-void _putchar(char n);
+int _putchar(char n);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
