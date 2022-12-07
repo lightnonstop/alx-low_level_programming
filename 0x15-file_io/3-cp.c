e86b1855c3b99113a6579c927ae0dab58f70ac3c
lightnonstop <umichaeledet003@gmail.com>
2022-12-07 10:19:09 +0100

    Copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-#define BUFF_SIZE 2048
+#define BUFF_SIZE 1024
 /**
-  * create_and_copy - Copies the content of a file to another file
-  * @oldfile: File to be copied from
-  * @newfile: File to be copied to
-  * Return: 1 if successful, 98 or 99 or 100 if there's a failure somewhere
+  * exit_mess - Handles exit code and exit messages
+  * @str: Exit message
+  * @file: File descriptor
+  * @exit_code: Exit value
   */
-int create_and_copy(char *oldfile, char *newfile)
+void exit_mess(char *str, char *file, int exit_code)
 {
-	int of, nf, rd, wr, clo, cln;
+	dprintf(STDERR_FILENO, str, file);
+	exit(exit_code);
+}
+
+/**
+  * _copy - Copies the content of a file to another file
+  * @file_from: File to be copied from
+  * @file_to: File to be copied to
+  */
+void _copy(char *file_from, char *file_to)
+{
+	int fd1, fd2, rd, wr;
 	char buff[BUFF_SIZE];
 
-	if (oldfile == NULL)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", oldfile);
-		exit(98);
-	}
-	of = open(oldfile, O_RDONLY);
-	if (of == -1)
-	{
-		dprintf(of, "Error: Can't read from file %s\n", oldfile);
-		exit(98);
-	}
-	nf = open(newfile, O_RDWR | O_CREAT | O_TRUNC, 0664);
-	rd = read(of, buff, BUFF_SIZE);
-	clo = close(of);
-	if (clo == -1)
-	{
-		printf("Error: Can't close fd %d\n", of);
-		exit(100);
-	}
-	wr = write(nf, buff, rd);
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+		exit_mess("Error: Can't read from file %s\n", file_from, 98);
+
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		exit_mess("Error: Can't write to %s\n", file_to, 99);
+
+
+	rd = read(fd1, buff, BUFF_SIZE);
+	if (rd == -1)
+		exit_mess("Error: Can't read from file %s\n", file_from, 98);
+
+	wr = write(fd2, buff, rd);
 	if (wr == -1)
+		exit_mess("Error: Can't write to %s\n", file_to, 99);
+
+	if (close(fd2) == -1)
 	{
-		printf("Error: Can't write to file %s\n", oldfile);
-		exit(99);
-	}
-	cln = close(nf);
-	if (cln == -1)
-	{
-		printf("Error: Can't close fd %d\n", nf);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
-	return (1);
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
 }
 /**
   * main - Starting point
@@ -57,9 +63,10 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	create_and_copy(argv[1], argv[2]);
+	_copy(argv[1], argv[2]);
+
 	return (0);
 }
