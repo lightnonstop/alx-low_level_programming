5559daf1dc0a82b6d39a61f40467c889d7036775
lightnonstop <umichaeledet003@gmail.com>
2023-01-13 14:20:31 +0000

    Determines the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * dlistint_len - Determines the number of elements in a double linked list
+  * @h: Points to the beginning of the linked list
+  * Return: Number of elements of the list or 0 if it fails
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	if (!h)
+		return (0);
+	while (h != NULL && h->n)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
