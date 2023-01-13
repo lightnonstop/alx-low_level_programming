5c2d8212420984ffb2dc3c3d648391093209a84f
lightnonstop <umichaeledet003@gmail.com>
2023-01-13 20:04:47 +0000

    Inserts a new node at a given position fixing error in code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,8 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = *h;
 		new->prev = NULL;
 		(*h)->prev = new;
-		*h = >n = n;
-		(*h)-
+		*h = new;
+		(*h)->n = n;
 		return (*h);
 	}
 	while (temp)
