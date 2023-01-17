76e867abf977bef47f11bdf5554c81a2246f0bb7
lightnonstop <umichaeledet003@gmail.com>
2023-01-17 00:48:21 +0000

    Updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lightnonstop/alx-low_level_programming/0x18-dynamic_libraries/jackhack.so
+wget -P /tmp https://github.com/lightnonstop/alx-low_level_programming/blob/master/0x18-dynamic_libraries/jackhack.so
 export LD_PRELOAD=/tmp/jackhack.so
